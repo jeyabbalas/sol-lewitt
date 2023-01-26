1eaa84d0f8bf7e2e6813eff764e9f8ee5a1c8802
Jeya Balaji Balasubramanian <jeyabbalas@gmail.com>
2023-01-26 12:55:30 -0500

    Move to D3

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-
-
 // const svgXmlNs = 'http://www.w3.org/2000/svg';
 const width = window.innerWidth;
 const height = window.innerHeight - 100;
 
-d3.select('body')
-    .append('h1')
+d3.select('body').append('h1')
     .text('Sol LeWitt');
 
 // Main SVG
@@ -25,6 +22,9 @@ const svg = d3.select('body')
 // peripheralMask.setAttribute('id', 'peripheral-mask');
 // svg.appendChild(peripheralMask);
 
+const peripheralMask = svg.append('mask')
+    .attr('id', 'peripheral-mask');
+
 // Visible part of the peripheral mask
 // const rectPeripheralMask = document.createElementNS(svgXmlNs, 'rect');
 // rectPeripheralMask.setAttribute('x', 0);
@@ -34,6 +34,11 @@ const svg = d3.select('body')
 // rectPeripheralMask.setAttribute('fill', 'white');
 // peripheralMask.appendChild(rectPeripheralMask);
 
+peripheralMask.append('rect')
+    .attr('width', width)
+    .attr('height', height)
+    .attr('fill', 'white');
+
 // Invisible part of the peripheral mask
 // const circlePeripheralMask = document.createElementNS(svgXmlNs, 'circle');
 // circlePeripheralMask.setAttribute('cx', width / 2);
@@ -41,6 +46,12 @@ const svg = d3.select('body')
 // circlePeripheralMask.setAttribute('r', 200);
 // circlePeripheralMask.setAttribute('fill', 'black');
 // peripheralMask.appendChild(circlePeripheralMask);
+
+peripheralMask.append('circle')
+    .attr('cx', width / 2)
+    .attr('cy', height / 2)
+    .attr('r', 200)
+    .attr('fill', 'black');
 
 // Vertical lines
 // for (let i = 0; i < 100; i++) {
@@ -53,10 +64,22 @@ const svg = d3.select('body')
 //     svg.appendChild(rect);
 // }
 
+svg.append('g')
+    .selectAll('rect')
+    .data(d3.range(100))
+    .join('rect')
+    .attr('x', d => d * 20)
+    .attr('width', 10)
+    .attr('height', height)
+    .attr('mask', 'url(#peripheral-mask)');
+
 // Center mask
 // const centerMask = document.createElementNS(svgXmlNs, 'mask');
 // centerMask.setAttribute('id', 'center-mask');
 // svg.appendChild(centerMask);
+
+const circleMask = svg.append('mask')
+    .attr('id', 'center-mask');
 
 // Invisible part of the center mask
 // const rectCenterMask = document.createElementNS(svgXmlNs, 'rect');
@@ -67,6 +90,11 @@ const svg = d3.select('body')
 // rectCenterMask.setAttribute('fill', 'black');
 // centerMask.appendChild(rectCenterMask);
 
+circleMask.append('rect')
+    .attr('width', width)
+    .attr('height', height)
+    .attr('fill', 'black');
+
 // Visible part of the center mask
 // const circleCenterMask = document.createElementNS(svgXmlNs, 'circle');
 // circleCenterMask.setAttribute('cx', width / 2);
@@ -74,6 +102,12 @@ const svg = d3.select('body')
 // circleCenterMask.setAttribute('r', 200);
 // circleCenterMask.setAttribute('fill', 'white');
 // centerMask.appendChild(circleCenterMask);
+
+circleMask.append('circle')
+    .attr('cx', width / 2)
+    .attr('cy', height / 2)
+    .attr('r', 200)
+    .attr('fill', 'white');
 
 // Horizontal lines
 // for (let i = 0; i < 100; i++) {
@@ -85,3 +119,12 @@ const svg = d3.select('body')
 //     rect.setAttribute('mask', 'url(#center-mask)');
 //     svg.appendChild(rect);
 // }
+
+svg.append('g')
+    .selectAll('rect')
+    .data(d3.range(100))
+    .join('rect')
+    .attr('y', d => d * 20)
+    .attr('height', 10)
+    .attr('width', width)
+    .attr('mask', 'url(#center-mask)');
